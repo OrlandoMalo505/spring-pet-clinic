0a4512e31d23bfcbdb202799f0b9287c46e750f6
User@DESKTOP-HFA7F9M.(none) <orlandomalo0@gmail.com>
2021-10-10 15:48:38 +0200

    Initial Project Add

---
+++ spring-peT-clinic/src/main/java/orlando/springframework/springpetclinic/SpringPeTClinicApplication.java
@@ -0,0 +1,13 @@
+package orlando.springframework.springpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPeTClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPeTClinicApplication.class, args);
+    }
+
+}
---
+++ spring-peT-clinic/src/test/java/orlando/springframework/springpetclinic/SpringPeTClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package orlando.springframework.springpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPeTClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
