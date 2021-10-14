a4fc0c632c0d971ce9a237bae80990e1a6edae20
User@DESKTOP-HFA7F9M.(none) <orlandomalo0@gmail.com>
2021-10-15 01:08:54 +0200

    added vets list and updated controllers

--- spring-peT-clinic/pet-clinic-web/src/main/java/orlando/springframework/springpetclinic/controllers/VetController.java
+++ spring-peT-clinic/pet-clinic-web/src/main/java/orlando/springframework/springpetclinic/controllers/VetController.java
@@ -1,12 +1,21 @@
 package orlando.springframework.springpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import orlando.springframework.springpetclinic.services.VetService;
 
 @Controller
 public class VetController {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 
