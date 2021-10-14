6a9b238f04cb24b2f945539c491f6ffff033c3dc
User@DESKTOP-HFA7F9M.(none) <orlandomalo0@gmail.com>
2021-10-15 00:52:46 +0200

    added owners list and updated controllers

--- spring-peT-clinic/pet-clinic-web/src/main/java/orlando/springframework/springpetclinic/controllers/OwnerController.java
+++ spring-peT-clinic/pet-clinic-web/src/main/java/orlando/springframework/springpetclinic/controllers/OwnerController.java
@@ -1,12 +1,24 @@
 package orlando.springframework.springpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import orlando.springframework.springpetclinic.services.OwnerService;
+
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+
     @RequestMapping({"","/","/index","/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+        model.addAttribute("owners",ownerService.findAll());
+
         return "owners/index";
 
     }
