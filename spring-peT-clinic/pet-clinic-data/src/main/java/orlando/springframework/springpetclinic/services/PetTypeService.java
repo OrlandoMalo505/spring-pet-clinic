e568681f5acf484933867bdeb48fdc23449cda8c
User@DESKTOP-HFA7F9M.(none) <orlandomalo0@gmail.com>
2021-10-17 00:27:51 +0200

    added PetTypeMap service. closes #19

---
+++ spring-peT-clinic/pet-clinic-data/src/main/java/orlando/springframework/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package orlando.springframework.springpetclinic.services;
+
+import orlando.springframework.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long>{
+}
---
+++ spring-peT-clinic/pet-clinic-data/src/main/java/orlando/springframework/springpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,37 @@
+package orlando.springframework.springpetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import orlando.springframework.springpetclinic.model.PetType;
+import orlando.springframework.springpetclinic.services.PetTypeService;
+
+import java.util.Set;
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType,Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteByID(id);
+
+    }
+}
