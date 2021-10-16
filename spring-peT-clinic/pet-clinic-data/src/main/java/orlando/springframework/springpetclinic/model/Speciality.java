881d3f7ff1c24740fe20a35a15b6a1bc2d924566
User@DESKTOP-HFA7F9M.(none) <orlandomalo0@gmail.com>
2021-10-17 00:08:29 +0200

    added specialities, added to vet. closes #17

---
+++ spring-peT-clinic/pet-clinic-data/src/main/java/orlando/springframework/springpetclinic/model/Speciality.java
@@ -0,0 +1,13 @@
+package orlando.springframework.springpetclinic.model;
+
+public class Speciality extends BaseEntity{
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
