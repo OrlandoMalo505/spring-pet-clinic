3ba13174969afac758b1c5d0583f2a909f5b1c09
User@DESKTOP-HFA7F9M.(none) <orlandomalo0@gmail.com>
2021-10-17 01:36:27 +0200

    added specialities map service impl.closes #21

---
+++ spring-peT-clinic/pet-clinic-data/src/main/java/orlando/springframework/springpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package orlando.springframework.springpetclinic.services;
+
+import orlando.springframework.springpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality,Long> {
+}
---
+++ spring-peT-clinic/pet-clinic-data/src/main/java/orlando/springframework/springpetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,37 @@
+package orlando.springframework.springpetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import orlando.springframework.springpetclinic.model.Speciality;
+import orlando.springframework.springpetclinic.services.SpecialitiesService;
+
+import java.util.Set;
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality,Long> implements SpecialitiesService{
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteByID(id);
+
+    }
+}
