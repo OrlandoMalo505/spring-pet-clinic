386df24add7ac5afb6acab4889ca38997a8744fd
User@DESKTOP-HFA7F9M.(none) <orlandomalo0@gmail.com>
2021-10-17 02:45:11 +0200

    fixed broken menu links

--- spring-peT-clinic/pet-clinic-web/src/main/java/orlando/springframework/springpetclinic/controllers/IndexController.java
+++ spring-peT-clinic/pet-clinic-web/src/main/java/orlando/springframework/springpetclinic/controllers/IndexController.java
@@ -5,8 +5,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-    @RequestMapping({"","/","index","index.html"})
-    public String index(){
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
         return "index";
+    }
+
+    @RequestMapping("/oups")
+    public String oupsHandler() {
+        return "notimplemented";
+
     }
 }
--- spring-peT-clinic/pet-clinic-web/src/main/java/orlando/springframework/springpetclinic/controllers/OwnerController.java
+++ spring-peT-clinic/pet-clinic-web/src/main/java/orlando/springframework/springpetclinic/controllers/OwnerController.java
@@ -22,4 +22,8 @@ public class OwnerController {
         return "owners/index";
 
     }
+    @RequestMapping("/find")
+    public String findOwners(){
+        return "notimplemented";
+    }
 }
