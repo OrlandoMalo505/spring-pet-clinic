fcd9d287ec220a5c49a9422c253be56b81885389
User@DESKTOP-HFA7F9M.(none) <orlandomalo0@gmail.com>
2021-10-19 00:32:41 +0200

    updated owners with JPA entity. closes #23

--- spring-peT-clinic/pet-clinic-data/src/main/java/orlando/springframework/springpetclinic/model/Person.java
+++ spring-peT-clinic/pet-clinic-data/src/main/java/orlando/springframework/springpetclinic/model/Person.java
@@ -1,7 +1,13 @@
 package orlando.springframework.springpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity {
+    @Column(name = "first_name")
     String firstName;
+    @Column(name = "last_name")
     String lastName;
 
     public String getFirstName() {
