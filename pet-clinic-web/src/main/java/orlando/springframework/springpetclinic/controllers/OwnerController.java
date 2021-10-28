304c55fbc78085ee0f6e6e6384cd6848654f0754
User@DESKTOP-HFA7F9M.(none) <orlandomalo0@gmail.com>
2021-10-28 18:29:31 +0200

    added display owners

--- pet-clinic-web/src/main/java/orlando/springframework/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/orlando/springframework/springpetclinic/controllers/OwnerController.java
@@ -2,7 +2,10 @@ package orlando.springframework.springpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import orlando.springframework.springpetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
@@ -26,4 +29,12 @@ public class OwnerController {
     public String findOwners(){
         return "notimplemented";
     }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId){
+        ModelAndView mav=new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
+    }
+
 }
