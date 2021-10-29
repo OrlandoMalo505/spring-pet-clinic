2db6bc5ea992ab9895227fca2a0a32728416dbd0
User@DESKTOP-HFA7F9M.(none) <orlandomalo0@gmail.com>
2021-10-29 02:49:32 +0200

    added visit controller

--- pet-clinic-data/src/main/java/orlando/springframework/springpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/orlando/springframework/springpetclinic/model/Pet.java
@@ -14,6 +14,20 @@ import java.util.Set;
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity{
+
+
+    @Builder
+    public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate, Set<Visit> visits) {
+        super(id);
+        this.name = name;
+        this.petType = petType;
+        this.owner = owner;
+        this.birthDate = birthDate;
+        if (visits == null || visits.size() > 0) {
+            this.visits = visits;
+        }
+    }
+
     @Column(name = "name")
     private String name;
     @ManyToOne
@@ -27,13 +41,5 @@ public class Pet extends BaseEntity{
     @OneToMany(cascade = CascadeType.ALL,mappedBy = "pet")
     private Set<Visit> visits=new HashSet<>();
 
-    @Builder
-    public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate, Set<Visit> visits) {
-        super(id);
-        this.name = name;
-        this.petType = petType;
-        this.owner = owner;
-        this.birthDate = birthDate;
-        this.visits = visits;
-    }
+
 }
