9d76faac9d51b4e1599a00164b66add0708c9351
User@DESKTOP-HFA7F9M.(none) <orlandomalo0@gmail.com>
2021-10-29 18:50:57 +0200

    fixed local date problem

--- pet-clinic-data/src/main/java/orlando/springframework/springpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/orlando/springframework/springpetclinic/model/Pet.java
@@ -1,6 +1,7 @@
 package orlando.springframework.springpetclinic.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -36,8 +37,11 @@ public class Pet extends BaseEntity{
     @ManyToOne
     @JoinColumn(name="owner_id")
     private Owner owner;
+
     @Column(name = "birth_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
+
     @OneToMany(cascade = CascadeType.ALL,mappedBy = "pet")
     private Set<Visit> visits=new HashSet<>();
 
