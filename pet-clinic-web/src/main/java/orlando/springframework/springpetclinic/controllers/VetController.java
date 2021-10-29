36658131286917ac6b9fc032fae4499995092d6e
User@DESKTOP-HFA7F9M.(none) <orlandomalo0@gmail.com>
2021-10-30 00:04:41 +0200

    added json endpoint for vets. closes #34

--- pet-clinic-web/src/main/java/orlando/springframework/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/orlando/springframework/springpetclinic/controllers/VetController.java
@@ -2,8 +2,13 @@ package orlando.springframework.springpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import orlando.springframework.springpetclinic.model.Vet;
 import orlando.springframework.springpetclinic.services.VetService;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -17,6 +22,11 @@ public class VetController {
     public String listVets(Model model){
         model.addAttribute("vets",vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+        return vetService.findAll();
     }
 
 }
